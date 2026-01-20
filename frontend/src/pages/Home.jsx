ce534e022011585894a90e7b91202557e0013822
Souvik Ghosh <ghoshsouvik1866@gmail.com>
2026-01-20 14:38:04 +0530

    useRef

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,71 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
+// import Footer from "../components/Footer"
+// import Header from "../components/Header"
+// import MainSection from "../components/MainSection"
+// import axios from "axios"
+// import toast from "react-hot-toast"
+
+// const Home = () => {
+
+//     const [todo, setTodo] = useState([])
+//     const [text, setText] = useState("")
+//     const [flag,setFlag]=useState(false)
+
+//     const getAll = async () => {
+//         try {
+//             const res = await axios.get(`http://localhost:8001/todo/getAll`)
+//             console.log(res.data.data)
+//             setTodo(res.data.data)
+
+//         } catch (error) {
+//             console.log("Data Not Found", error)
+//         }
+//     }
+
+//     const handleTodo = async (text) => {
+//         const data = {
+//             "title": text
+//         }
+//         try {
+//             setFlag(false)
+//             const res = await axios.post(`http://localhost:8001/todo/create`, data)
+//             console.log(res)
+//             toast.success(res.data.message)
+//             setFlag(true)
+//             setText('')
+
+//         } catch (error) {
+//             console.log("Data Not Created",error)
+//             toast.error(error.response.data.errors || error.response.data.message)
+//             setText("")
+//         }
+//     }
+
+//     useEffect(() => {
+//         getAll()
+//     }, [flag])
+
+//     return (
+//         <div className="text-center pt-20">
+//             <Header />
+//             <div>
+//                 <input onChange={(e) => setText(e.target.value)} value={text} type="text" placeholder="add todo" />
+//                 <button onClick={() => handleTodo(text)}>Add</button>
+//             </div>
+//             <MainSection todo={todo} getAll={getAll} />
+//             <Footer />
+
+
+//         </div>
+//     )
+// }
+
+// export default Home
+
+
+
+
+import { useEffect, useRef, useState } from "react"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import MainSection from "../components/MainSection"
@@ -10,6 +77,7 @@ const Home = () => {
     const [todo, setTodo] = useState([])
     const [text, setText] = useState("")
     const [flag,setFlag]=useState(false)
+    const inputRef = useRef(null)
 
     const getAll = async () => {
         try {
@@ -22,17 +90,20 @@ const Home = () => {
         }
     }
 
-    const handleTodo = async (text) => {
+    
+    const handleTodo = async () => {
         const data = {
-            "title": text
-        }
+            "title": inputRef.current.value }
+
         try {
+            console.log(inputRef)
             setFlag(false)
             const res = await axios.post(`http://localhost:8001/todo/create`, data)
             console.log(res)
             toast.success(res.data.message)
             setFlag(true)
-            setText('')
+            // setText('')
+            inputRef.current.value = ''
 
         } catch (error) {
             console.log("Data Not Created",error)
@@ -49,7 +120,7 @@ const Home = () => {
         <div className="text-center pt-20">
             <Header />
             <div>
-                <input onChange={(e) => setText(e.target.value)} value={text} type="text" placeholder="add todo" />
+                <input ref={inputRef} type="text" placeholder="add todo" />
                 <button onClick={() => handleTodo(text)}>Add</button>
             </div>
             <MainSection todo={todo} getAll={getAll} />
